6f004ded1391c7b92e6dd906e7bd37e9b066ee8b
SHIMOMURA Sho <graueule@gmail.com>
2011-08-20 20:45:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watage::Application.config.secret_token = '5a6e7c38a68467897b348b0768c33013fa1b7731a7e68f8c2646f097553fb21e267deee6977badb37672b243a430f5676b85d37400727bf6f9c3ff60fe89ea7a'
